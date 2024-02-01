4300bd71645e31704c7a1a1835e4f7929820a0cd
Amr ALHOSSARY <aalhossary@wesleyan.edu>
2024-02-01 07:20:03 -0600

    using the foldtree to adjust MonteCarlo moves

--- source/src/apps/pilot/aalhossary/bootcamp.cc
+++ source/src/apps/pilot/aalhossary/bootcamp.cc
@@ -7,6 +7,7 @@
 // (c) For more information, see http://www.rosettacommons.org. Questions about this can be
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
+#include "core/pose/variant_util.hh"
 #include <iostream>
 
 #include <basic/options/keys/in.OptionKeys.gen.hh>
@@ -26,26 +27,33 @@
 #include <numeric/random/random.hh>
 #include <protocols/moves/MonteCarlo.hh>
 #include <utility/pointer/owning_ptr.hh>
+#include <core/conformation/Conformation.hh>
+#include <protocols/bootcamp/fold_tree_from_ss.hh>
 
 
 static constexpr const int NUM_ITEERATIONS = 1000;
 
-int main(int argc, char** argv) {
-	std::cout << "Hello World!" << std::endl;
+int main(int argc, char **argv) {
+    std::cout << "Hello World!" << std::endl;
 
-	devel::init( argc, argv );
-	utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
-	if ( filenames.size() > 0 ) {
-		std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
-	} else {
-		std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
-		return 1;
-	}
+    devel::init(argc, argv);
+    utility::vector1<std::string> filenames = basic::options::option[basic::options::OptionKeys::in::file::s].value();
+    if (filenames.size() > 0) {
+        std::cout << "You entered: " << filenames[1] << " as the PDB file to be read" << std::endl;
+    } else {
+        std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
+        return 1;
+    }
     core::pose::PoseOP mypose = core::import_pose::pose_from_file(filenames[1]);
     core::scoring::ScoreFunctionOP sfxn = core::scoring::get_score_function();
     core::Size N = mypose->size();
-    core::Real score = sfxn->score( *mypose );
+    core::Real score = sfxn->score(*mypose);
     std::cout << "score =" << score << std::endl;
+
+    auto conf = mypose->conformation();
+    conf.fold_tree(*protocols::bootcamp::fold_tree_from_ss(*mypose));
+    core::pose::correctly_add_cutpoint_variants(*mypose);
+    sfxn->set_weight(core::scoring::ScoreType::linear_chainbreak, 1.0);
 
     protocols::moves::MonteCarlo monteCarlo = protocols::moves::MonteCarlo(*mypose, *sfxn, 1.);
     core::kinematics::MoveMap mm;
